c583aa704b708d95d3a6752af118db975223560a
edgardopina <edgardopina@yahoo.com>
2021-10-19 00:42:21 -0600

    Added schema.sql, server.js, connection.js, User model as User.js, index.js

---
+++ models/User.js
@@ -0,0 +1,58 @@
+// import Model class and Datatypes object
+// This Model class is what we create our own models from using the extends keyword so
+// User inherits all of the functionality that the Model class has.
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create User model
+class User extends Model {}
+
+// define table columns and configuration
+// .init() method to initialize the model's data and configuration, passing in two objects
+// as arguments: The first object will define the columns and data types for those columns.
+// The second object it accepts configures certain options for the table.
+User.init(
+   // TODO - table configuration options go here  (https://sequelize.org/v5/manual/models-definition.html#configuration)
+   {
+      // id column
+      id: {
+         type: Datatypes.INTEGER, // use the special Sequelize DataTypes object provide what type of data it is
+         allowNull: false, // this is the equivalent of SQL's `NOT NULL` option
+         primaryKey: true, // instruct that this is the Primary Key
+         autoIncrement: true, // turn on auto increment
+      },
+      // username column
+      username: {
+         type: Datatypes.STRING, // use the special Sequelize DataTypes object provide what type of data it is
+         allowNull: false, // this is the equivalent of SQL's `NOT NULL` option
+      },
+      // email column
+      email: {
+         type: Datatypes.STRING, // use the special Sequelize DataTypes object provide what type of data it is
+         allowNull: false, // this is the equivalent of SQL's `NOT NULL` option
+         unique: true, // there cannot be any duplicate email values in this table
+         // IF allowNull IS SET TO false, we can run our data through validators before creating the table data
+         validate: {
+            isEmail: true,
+         },
+      },
+      // password column
+      password: {
+         type: Datatypes.STRING, // use the special Sequelize DataTypes object provide what type of data it is
+         allowNull: false, // this is the equivalent of SQL's `NOT NULL` option
+         // IF allowNull IS SET TO false, we can run our data through validators before creating the table data
+         validate: {
+            len: [4], // password must be at least four characters long
+         },
+      },
+   },
+   {
+      sequelize, // pass in our imported sequelize connection (the direct connection to our database)
+      timestamps: false, // don't automatically create createdAt/updatedAt timestamp fields
+      freezeTableName: true, // do NOT PLURALIZE name of database table
+      underscored: true, // USE UNDERSCORES INSTEAD OF CAMEL-CASING (i.e. `comment_text` and not `commentText`)
+      modelName: 'user', // make it so our model name stays lowercase in the database
+   }
+);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+
+// All this file is responsible for right now is importing the User model and exporting an object
+// with it as a property.It seems unnecessary at the moment, but doing this now will set us up for 
+// future growth of the application.
+
+module.exports = { User };
