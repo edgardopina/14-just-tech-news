6c32da02ef4df74b389ddd7a32a1cd31a2c20245
edgardopina <edgardopina@yahoo.com>
2021-10-20 22:56:21 -0600

    Updated login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,6 +55,28 @@ router.post('/', (req, res) => {
       });
 });
 
+// POST /api/users/login
+router.post('/login', (req, res) => {
+   User.findOne({
+      where: {
+         email: req.body.email,
+      },
+   }).then(dbUserData => {
+      if (!dbUserData) {
+         res.status(404).json({ message: 'No user found with that email address!' });
+         return;
+      }
+      // res.json({ user: dbUserData });
+      //verify user
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+         res.status(400).json({ message: 'Incorrect password!' });
+         return;
+      }
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+   });
+});
+
 // PUT /api/isers/1
 router.put('/:id', (req, res) => {
    // expects {username: 'Lernantino', email: 'learnantino@gmail.com', password: 'password1234'}
