f943769c982eca15ef48e529458cf1fe1ac76415
edgardopina <edgardopina@yahoo.com>
2021-10-22 22:36:47 -0600

    Updated post-routes.js and user-routes.js to include the Comment model

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Vote, Comment } = require('../../models');
 const sequelize = require('sequelize');
 
 // GET /api/posts - all posts
@@ -19,6 +19,14 @@ router.get('/', (req, res) => {
       ],
       order: [['created_at', 'DESC']],
       include: [
+         {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+               model: User,
+               attributes: ['username'],
+            },
+         },
          {
             model: User,
             attributes: ['username'],
@@ -50,6 +58,14 @@ router.get('/:id', (req, res) => {
          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
       ],
       include: [
+         {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+               model: User,
+               attributes: ['username'],
+            },
+         },
          {
             model: User,
             attributes: ['username'],
