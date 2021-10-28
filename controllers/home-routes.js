dd68ac9bd870ae3922bd1e440c53ee95c908f868
edgardopina <edgardopina@yahoo.com>
2021-10-28 00:34:42 -0600

    Created single-post.hbs template, added Post.findOne() route. Tested

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -54,4 +54,62 @@ router.get('/login', (req, res) => {
    res.render('login');
 });
 
+//
+router.get('/post/:id', (req, res) => {
+   Post.findOne({
+      where: {
+         id: req.params.id,
+      },
+      attributes: [
+         'id',
+         'post_url',
+         'title',
+         'created_at',
+         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+         {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+               model: User,
+               attributes: ['username']
+            }
+         },
+         {
+            model: User,
+            attributes: ['username'],
+         }
+      ]
+   })
+      .then(dbPostData => {
+         if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+         }
+
+         const post = dbPostData.get({ plain: true }); // serialize the data
+         res.render('single-post', { post }); // pass data to the template
+      })
+      .catch(err => {
+         console.error(err);
+         res.status(500).json(err);
+      });
+});
+
+// const post = {
+//    id: 1,
+//    post_url: 'https://handlebarsjs.com/guide/',
+//    title: 'Handlebars Docs',
+//    created_at: new Date(),
+//    vote_count: 10,
+//    comments: [{}, {}],
+//    user: {
+//       username: 'test_user',
+//    },
+// };
+
+// res.render('single-post', { post });
+// });
+
 module.exports = router;
